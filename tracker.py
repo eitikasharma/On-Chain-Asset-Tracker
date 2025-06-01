7eb579fe6cdbc6da4bc042bb045832d348d1d555
Eitika Sharma <44219225+eitikasharma@users.noreply.github.com>
2025-06-01 21:47:01 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import plotly.express as px
 
-# Load the CSV you downloaded from Dune
+# Load the CSV file
 df = pd.read_csv("erc20_data.csv")
 
-# Make sure the date column is read correctly
+# Make sure that the date column is read correctly
 df['date'] = pd.to_datetime(df['date'])
 
 # Show line chart of volume over time
